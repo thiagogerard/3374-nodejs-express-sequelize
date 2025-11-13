9bd9b2cc12e4d50fe63a783169b80c92127a3495
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-11-13 14:59:16 -0800

    feat: implement services and controllers for Curso, Matricula, and Categoria entities; add corresponding routes

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -1,14 +1,17 @@
 const { Router } = require('express');
-const PessoaControler = require('../controllers/PessoaController.js');
+const PessoaController = require('../controllers/PessoaController.js');
+const MatriculaController = require('../controllers/MatriculaController.js');
 
-const pessoaControler = new PessoaControler();
+const pessoaController = new PessoaController();
+const matriculaController = new MatriculaController();
 
 const router = Router();
 
-router.get('/pessoas', (req, res) => pessoaControler.pegaTodos(req, res));
-router.get('/pessoas/:id', (req, res) => pessoaControler.pegaPorId(req, res));
-router.post('/pessoas', (req, res) => pessoaControler.criaNovo(req, res));
-router.put('/pessoas/:id', (req, res) => pessoaControler.atualiza(req, res));
-router.delete('/pessoas/:id', (req, res) => pessoaControler.exclui(req, res));
+router.get('/pessoas', (req, res) => pessoaController.pegaTodos(req, res));
+router.get('/pessoas/:id', (req, res) => pessoaController.pegaPorId(req, res));
+router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
+router.put('/pessoas/:id', (req, res) => pessoaController.atualiza(req, res));
+router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
+router.post('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.criaNovo(req, res));
 
 module.exports = router;
