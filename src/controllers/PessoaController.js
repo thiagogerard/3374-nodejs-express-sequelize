912189c490e552d43d1ffca70670fa0d2f41793c
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-11-13 19:17:09 -0800

    feat: add functionality to retrieve student enrollments by student ID

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -7,6 +7,16 @@ class PessoaControler extends Controller{
     constructor() {
         super(pessoaServices);
     }
+
+    async pegaMatriculas(req, res) {
+        const { estudanteId } = req.params;
+        try {
+            const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+            return res.status(200).json(listaMatriculas);
+        }catch (err) {
+            //erro
+        }
+    }
 }
 
 module.exports = PessoaControler;
