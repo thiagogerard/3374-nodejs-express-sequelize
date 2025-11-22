7aaa51285822d2d4563e38088eb8ae38f95dae67
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-11-21 16:45:13 -0800

    feat: add soft delete support to Pessoas, Categorias, Cursos, and Matriculas models

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -27,6 +27,11 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Pessoa',
     tableName: 'pessoas',
     paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true,
+      }
+    }
   });
   return Pessoa;
 };
