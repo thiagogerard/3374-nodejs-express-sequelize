9b04525b37f4b8d01579926a0d6925e6a96a4f22
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-12-30 12:26:23 -0800

    feat: add endpoint to retrieve courses with enrollment count and limit

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,9 +1,10 @@
+const Sequelize = require('sequelize');
 const Controller = require('./Controller.js');
 const MatriculaServices = require('../services/MatriculaServices.js');
 
 const matriculaServices = new MatriculaServices();
 
-class MatriculaController extends Controller{
+class MatriculaController extends Controller {
     constructor() {
         super(matriculaServices);
     }
@@ -11,11 +12,36 @@ class MatriculaController extends Controller{
     async pegaMatriculasPorEstudante(req, res) {
         const { estudante_id } = req.params;
         try {
-            const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros({
-                estudante_id: Number(estudante_id),
-                status: 'matriculado'
-            });
+            const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros(
+                {
+                    where: {
+                        estudante_id: Number(estudante_id),
+                        status: 'matriculado'
+                    },
+                    limit: 2,
+                    order: [['id', 'DESC']]
+                }
+            );
             return res.status(200).json(listaMatriculasPorEstudante.count);
+        } catch (err) {
+            return res.status(500).json({ erro: err.message });
+        }
+    }
+
+    async pegaCursosLotados(req, res) {
+        const lotacaoCurso = 2;
+        try {
+            const cursosLotados = await matriculaServices.pegaEContaRegistros(
+                {
+                    where: {
+                        status: 'matriculado'
+                    },
+                    attributes: ['curso_id'],
+                    group: ['curso_id'],
+                    having: Sequelize.literal(`count(curso_id) >= ${lotacaoCurso}`)
+                }
+            );
+            return res.status(200).json(cursosLotados.count);
         } catch (err) {
             return res.status(500).json({ erro: err.message });
         }
--- src/services/Services.js
+++ src/services/Services.js
@@ -21,8 +21,8 @@ class Sevices {
         return dataSource[this.model].findOne({ where: { ...where } });
     }
 
-    async pegaEContaRegistros(where) {
-        return dataSource[this.model].findAndCountAll({ where: { ...where } });
+    async pegaEContaRegistros(options) {
+        return dataSource[this.model].findAndCountAll({ ...options });
     }
 
     async criaRegistro(dados) {
